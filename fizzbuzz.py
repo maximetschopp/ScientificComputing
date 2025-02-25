17635594f6b14f75b71d96c9de6d5540c6c1333e
Maxime Tschopp <maxime.tschopp@icloud.com>
2025-02-25 11:30:45 +0100

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,4 @@
-def fizzbuzz(start, stop):
+def fizzbuzzOld(start, stop):
     output = ""
     for i in range (start, stop +1):
         if i % 3 == 0:
@@ -10,5 +10,10 @@ def fizzbuzz(start, stop):
         output += ", "
     return output
     
+def fizzbuzz(start, stop):
+    output = ""
+    for i in range (start, stop + 1):
+        output += ( "fizz" * (i % 3 == 0) + "buzz" * (i % 5 == 0) or str(i) ) + ", "
+    return output[:-2] # remove trailing comma and whitespace
     
 print(fizzbuzz(0,100))
